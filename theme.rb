3e4d22e1e7c8cad96e93178fbec6edd73b2b6815
okkez <okkez000@gmail.com>
2016-08-29 00:03:44 +0900

    Apply lightning

--- theme.rb
+++ theme.rb
@@ -1,7 +1,19 @@
 include_theme("rurema")
 
+@lightning_talk_proc_name = "lightning-rurema"
+@lightning_talk_as_large_as_possible = true
+include_theme("lightning-talk-toolkit")
+
 match(TitleSlide, Title) do |titles|
   prop_set("weight", "normal")
+end
+
+match(Slide) do |slides|
+  slides.each do |slide|
+    if slide.lightning_talk?
+      slide.lightning_talk
+    end
+  end
 end
 
 match(Slide, HeadLine) do |head_lines|
